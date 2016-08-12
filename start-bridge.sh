8e9f1f77acb20bb1d6fb477d9c6fa011bcb75023
Doug Anson <doug.anson@arm.com>
2016-08-12 09:57:49 -0500

    updated

--- start-bridge.sh
+++ start-bridge.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-IP=192.168.1.230
+IP="`ip route get 8.8.8.8 | awk '{print $NF; exit}'`"
 
 set -x
 
-docker run -d -p ${IP}:8883:8883 -p ${IP}:28519:28519 -p ${IP}:28520:28520 -p ${IP}:22:22 -p ${IP}:8234:8234 -t mbed/connector-bridge  /home/arm/start_instance.sh
+docker run -d -p ${IP}:1883:1883 -p ${IP}:8883:8883 -p ${IP}:28519:28519 -p ${IP}:28520:28520 -p ${IP}:22:22 -p ${IP}:8234:8234 -t mbed/connector-bridge  /home/arm/start_instance.sh
